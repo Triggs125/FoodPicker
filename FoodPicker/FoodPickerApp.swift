692e30f05eb9c2ce972c92a7a8dbce79f5d70331
Triggs125 <tannerdriggers@gmail.com>
2022-01-25 19:46:34 -0600

    IOS init

---
+++ FoodPicker/FoodPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPickerApp.swift
+//  FoodPicker
+//
+//  Created by Tanner Driggers on 1/23/22.
+//
+
+import SwiftUI
+
+@main
+struct FoodPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            OverView()
+        }
+    }
+}
