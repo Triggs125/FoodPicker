9185bf4da99ceec2371d4c61c9d9138e964acdc4
Triggs125 <tannerdriggers@gmail.com>
2022-02-01 01:48:24 -0600

    Remove algolia key

--- FoodPicker_expo/components/Algolia/SearchAlgolia.jsx
+++ FoodPicker_expo/components/Algolia/SearchAlgolia.jsx
@@ -4,6 +4,7 @@ import ConnectedInfiniteHits from "./ConnectedInfiniteHits";
 import ConnectedSearchBox from "./ConnectedSearchBox";
 import { View } from "react-native";
 import algoliasearch from "algoliasearch";
+import { algoliaSecret } from "../../config";
 
 const VirtualRefinementList = connectRefinementList(() => null);
 
@@ -13,7 +14,7 @@ class SearchAlgolia extends Component {
 
     this.state = {
       searchState: {},
-      searchClient: algoliasearch('281QB5GMK9', 'c34ee83f45934b56d0e2d8155e65d54c')
+      searchClient: algoliasearch('281QB5GMK9', algoliaSecret)
     }
 
     this.onSearchStateChange = this.onSearchStateChange.bind(this);
