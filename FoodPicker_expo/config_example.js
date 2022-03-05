abc4ec901a993c65e89ebc603045822e869ee28c
Triggs125 <tannerdriggers@gmail.com>
2022-03-05 13:27:21 -0600

    Add config example

---
+++ FoodPicker_expo/config_example.js
@@ -0,0 +1,18 @@
+/* Copy this file and rename it to config.js */
+
+// Config from firebase webapp
+export const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
+
+// API Key for google maps and nearby search
+export const GOOGLE_MAPS_API_KEY = "";
+
+// API Key for google place details
+export const PLACE_DETAILS_API_KEY = "";
