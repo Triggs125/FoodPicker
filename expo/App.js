8554528e900b00d730b44c1151312c048c3b150d
Triggs125 <tannerdriggers@gmail.com>
2022-03-05 00:07:28 -0600

    Add Sharing Add Forgot Password Functionality Fixup some formatting

---
+++ expo/App.js
@@ -0,0 +1,284 @@
+import 'react-native-gesture-handler';
+import React, { useState } from 'react';
+import { StyleSheet, View, Image, LogBox } from 'react-native';
+import { Button, Icon, Text } from 'react-native-elements';
+import { NavigationContainer, ThemeProvider } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import * as SplashScreen from 'expo-splash-screen';
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { collection, getDocs, getFirestore, query, where } from 'firebase/firestore';
+import { firebaseConfig } from './config';
+import ThemeColors from './assets/ThemeColors';
+import { UserColors } from './ColorContext';
+import { onAuthStateChanged } from "firebase/auth";
+import Constants from 'expo-constants';
+import * as Linking from 'expo-linking';
+
+import Settings from './components/Settings';
+import Account from './components/Account/Account';
+import CreateAccount from './components/Account/CreateAccount';
+import LobbyPicker from './components/Lobby/LobbyPicker';
+import LobbyView from './components/Lobby/LobbyView';
+import MakeSelections from './components/Selections/MakeSelections';
+import EditFoodProfile from './components/FoodProfile/EditFoodProfile';
+import UserSelections from './components/Selections/UserSelections';
+import PlaceDetails from './components/Selections/PlaceDetails';
+import LobbyCreator from './components/Lobby/LobbyCreator';
+import LoadingSpinner from './components/LoadingSpinner';
+
+import {
+  AdMobBanner,
+  setTestDeviceIDAsync,
+} from 'expo-ads-admob';
+import AccountEdit from './components/Account/AccountEdit';
+import ForgotPassword from './components/Account/ForgotPassword';
+
+const Stack = createStackNavigator();
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const prefix = Linking.createURL('/');
+
+export default function App() {
+  const [userColors, setUserColors] = useState({});
+  const [loading, setLoading] = useState(true); 
+  const [user, setUser] = useState();
+  const [lobbyData, setLobbyData] = useState();
+  const [userLobbies, setUserLobbies] = useState();
+
+  const navColors = {
+    colors: {
+      primary: ThemeColors.textTitle, // selected nav tab
+      background: ThemeColors.background,
+      card: ThemeColors.card,
+      text: ThemeColors.text,
+      border: ThemeColors.border,
+      notification: ThemeColors.button,
+    }
+  }
+  const themeProviderColors = {
+    Button: {
+      buttonStyle: {
+        backgroundColor: ThemeColors.button
+      },
+      disabledStyle: {
+        backgroundColor: ThemeColors.disabledButton
+      },
+      disabledTitleStyle: {
+        color: '#fff',
+      }
+    },
+    Input: {
+      inputContainerStyle: {
+        borderColor: ThemeColors.border
+      },
+      inputStyle: {
+        color: ThemeColors.border
+      }
+    },
+    Overlay: {
+      overlayStyle: {
+        backgroundColor: ThemeColors.background,
+      }
+    },
+    Text: {
+      style: {
+        color: ThemeColors.button,
+      }
+    },
+    ListItem: {
+      containerStyle: {
+        backgroundColor: ThemeColors.card,
+        borderColor: ThemeColors.border,
+      }
+    }
+  }
+
+  const linking = {
+    prefixes: [prefix, 'https://my-food-picker.web.app/'],
+  };
+
+  SplashScreen.preventAutoHideAsync();
+  SplashScreen.hideAsync();
+  const navigationRef = React.useRef();
+  
+  // Ignore log notification by message
+  LogBox.ignoreLogs([
+    'AsyncStorage',
+    'Setting a timer for a long period of time'
+  ]);
+
+  const adUnitId = !Constants.platform.web && __DEV__
+    ? "ca-app-pub-3940256099942544/6300978111"
+    : Constants.platform.android
+      ? "ca-app-pub-1885494348989912/3971948721"
+      : "ca-app-pub-1885494348989912/2737353131";
+
+  // const adUnitId = "ca-app-pub-1885494348989912/3971948721";
+
+  // console.log("Ad Unit", adUnitId);
+
+  __DEV__
+  ? setTestDeviceIDAsync('EMULATOR')
+    .then(() => {
+      console.log("Test Device ID set");
+      setLoading(false);
+    })
+  : setLoading(false);
+
+  onAuthStateChanged(auth, (authUser) => {
+    if (!authUser) {
+      setUser(null);
+      return;
+    }
+    getDocs(query(collection(db, 'users'), where('uid', '==', authUser.uid)))
+    .then(docs => {
+      const user = docs.docs[0].data();
+      authUser.firstName = user.firstName;
+      authUser.lastName = user.lastName;
+      setUser(authUser);
+    })
+    .catch(err => {
+      console.log("App:onAuthStateChanged", err);
+      setUser(authUser)
+    })
+  });
+
+  return loading ? (
+    <LoadingSpinner />
+  ) : (
+    <ThemeProvider theme={themeProviderColors}>
+      <UserColors.Provider value={{ userColors, setUserColors }}>
+        <NavigationContainer
+          ref={navigationRef}
+          theme={navColors}
+          linking={linking}
+        >
+          <Stack.Navigator
+            initialRouteName="Account"
+            screenOptions={(props) => {
+              return {
+                headerRight: () => (
+                  <Button
+                    icon={{
+                      name: 'user-circle',
+                      type: 'font-awesome',
+                      color: 'black',
+                      marginRight: 8
+                    }}
+                    style={{ fontSize: 24 }}
+                    buttonStyle={{ backgroundColor: 'transparent' }}
+                    onPress={() => props.navigation.navigate('Account')}
+                  />
+                ),
+                headerTitleAlign: 'center',
+              }
+            }}
+          >
+            <Stack.Screen
+              name="Account"
+              options={{ headerTitle: 'Account', headerRight: () => {<></>} }}
+            >
+              {props => <Account {...props} user={user} auth={auth} db={db} />}
+            </Stack.Screen>
+            <Stack.Screen
+              name="CreateAccount"
+              options={{ headerTitle: 'Create an Account', headerRight: () => {<></>} }}
+            > 
+              {props => <CreateAccount {...props} user={user} auth={auth} db={db} />}
+            </Stack.Screen>
+            <Stack.Screen
+              name="ForgotPassword"
+              options={{ headerTitle: 'Reset Password', headerRight: () => {<></>} }}
+            >
+              {props => <ForgotPassword {...props} user={user} auth={auth} db={db} />}
+            </Stack.Screen>
+            {
+              // Cannot see these screens until the user is logged in
+              user && (
+                <>
+                  <Stack.Screen
+                    name="AccountEdit"
+                    options={{ headerTitle: 'Create an Account' }}
+                  > 
+                    {props => <AccountEdit {...props} setUser={setUser} user={user} auth={auth} db={db} />}
+                  </Stack.Screen>
+                  <Stack.Screen
+                    name="Settings"
+                    options={{ headerTitle: "Settings" }}
+                  >
+                    {props => <Settings {...props} user={user} auth={auth} db={db} />}
+                  </Stack.Screen>
+                  <Stack.Screen
+                    name="LobbyPicker"
+                    options={{ headerTitle: "Join or Start a Lobby" }}
+                  >
+                    {props => <LobbyPicker {...props} userLobbies={userLobbies} user={user} auth={auth} db={db} />}
+                  </Stack.Screen>
+                  <Stack.Screen
+                    name="LobbyCreator"
+                    options={{ headerTitle: "Create a Lobby" }}
+                  >
+                    {props => <LobbyCreator {...props} user={user} auth={auth} db={db} />}
+                  </Stack.Screen>
+                  <Stack.Screen
+                    name="LobbyView"
+                    options={{ headerTitle: "Lobby" }}
+                  >
+                    {props => <LobbyView {...props} user={user} auth={auth} db={db} setLobbyData={setLobbyData} />}
+                  </Stack.Screen>
+                  <Stack.Screen
+                    name="MakeSelections"
+                    options={{
+                      headerTitle: "Make Selections",
+                      headerTitleAlign: 'center'
+                    }}
+                  >
+                    {props => <MakeSelections {...props} user={user} auth={auth} db={db} lobbyData={lobbyData} />}
+                  </Stack.Screen>
+                  <Stack.Screen
+                    name="UserSelections"
+                    options={{
+                      headerTitle: "Selections",
+                      headerTitleAlign: 'center'
+                    }}
+                  >
+                    {props => <UserSelections {...props} user={user} auth={auth} db={db} lobbyData={lobbyData} />}
+                  </Stack.Screen>
+                  <Stack.Screen
+                    name="EditFoodProfile"
+                    options={{ headerTitle: "Edit Food Profile" }}
+                  >
+                    {props => <EditFoodProfile {...props} user={user} auth={auth} db={db} />}
+                  </Stack.Screen>
+                  <Stack.Screen
+                    name="PlaceDetails"
+                    options={{ headerTitle: "Place Details" }}
+                  >
+                    {props => <PlaceDetails {...props} user={user} auth={auth} db={db} lobbyData={lobbyData} />}
+                  </Stack.Screen>
+                </>
+              )
+            }
+          </Stack.Navigator>
+        </NavigationContainer>
+        <View>
+          <AdMobBanner
+            adUnitId={adUnitId}
+            bannerSize={'fullBanner'}
+            servePersonalizedAds={false}
+            onAdViewDidReceiveAd={() => {
+              console.log('Advert loaded');
+            }}
+            onDidFailToReceiveAdWithError={(error) => {
+              console.error('Advert failed to load: ', error);
+            }}
+          />
+        </View>
+      </UserColors.Provider>
+    </ThemeProvider>
+  )
+}
---
+++ expo/components/Selections/MakeSelections.jsx
@@ -0,0 +1,268 @@
+import { Component } from "react";
+import { Dimensions, ScrollView, View } from "react-native";
+import { Card, Icon, Input, Text } from 'react-native-elements';
+import { HeaderHeightContext } from '@react-navigation/elements';
+// import FoodProfile from "../FoodProfile/FoodProfile";
+// import GoogleFoodSearch from "./GoogleFoodSearch";
+import FoodChoices from "./FoodChoices";
+import FoodPageNavigation from "./FoodPageNavigation";
+import LoadingSpinner from "../LoadingSpinner";
+import Constants from 'expo-constants';
+import { getDoc, doc } from "firebase/firestore";
+import { PLACE_DETAILS_API_KEY, NEARBY_SEARCH_API_KEY } from "../../config";
+
+class MakeSelections extends Component {
+  constructor(props) {
+    super(props);
+    const offset = Constants.platform.android ? 35 : 0;
+    const adBannerHeight = 60;
+    const screenHeight = Dimensions.get('screen').height - offset - adBannerHeight;
+    const maxNumberOfSelections = 5
+
+    this.state = {
+      screenHeight: screenHeight,
+      selectedFoodProfile: undefined,
+      googleSearchText: "",
+      selectedFoodChoices: [],
+      foodChoices: [],
+      choicesPageIndex: 0,
+      nextPageToken: undefined,
+      tokenPage: -1,
+      loading: true,
+      maxNumberOfSelections: maxNumberOfSelections,
+    }
+
+    this.setSelectedFoodProfile = this.setSelectedFoodProfile.bind(this);
+    this.setGoogleSearchText = this.setGoogleSearchText.bind(this);
+    this.setChoices = this.setChoices.bind(this);
+    this.addFoodChoice = this.addFoodChoice.bind(this);
+    this.removeFoodChoice = this.removeFoodChoice.bind(this);
+    this.nextChoicesPage = this.nextChoicesPage.bind(this);
+    this.lastChoicesPage = this.lastChoicesPage.bind(this);
+    this.clearSelections = this.clearSelections.bind(this);
+    this.getUserFoodSelections = this.getUserFoodSelections.bind(this);
+  }
+
+  componentDidMount() {
+    const { lobbyData } = this.props;
+    this.componentFocusUnsub = this.props.navigation.addListener('focus', () => {
+      this.fetchNearestPlacesFromGoogle();
+      this.getUserFoodSelections();
+    });
+    if (lobbyData) {
+      this.fetchNearestPlacesFromGoogle();
+      this.getUserFoodSelections();
+    }
+  }
+
+  componentWillUnmount() {
+    this.componentFocusUnsub && this.componentFocusUnsub();
+  }
+
+  setSelectedFoodProfile(selectedFoodProfile) {
+    this.setState({ selectedFoodProfile });
+  }
+
+  setGoogleSearchText(googleSearchText) {
+    this.setState({ googleSearchText, choicesPageIndex: 0 });
+  }
+
+  setChoices(foodChoices) {
+    this.setState({ foodChoices });
+  }
+
+  addFoodChoice(foodChoice) {
+    const { selectedFoodChoices, maxNumberOfSelections } = this.state;
+    if (selectedFoodChoices.includes(foodChoice) || selectedFoodChoices.length >= maxNumberOfSelections) return;
+    let newFoodChoices = selectedFoodChoices;
+    newFoodChoices.push(foodChoice);
+    this.setState({ selectedFoodChoices: newFoodChoices });
+  }
+
+  removeFoodChoice(foodChoice) {
+    const { selectedFoodChoices } = this.state;
+    let newFoodChoices = selectedFoodChoices;
+    newFoodChoices = newFoodChoices.filter(choice => foodChoice.id !== choice.id);
+    this.setState({ selectedFoodChoices: newFoodChoices });
+  }
+
+  clearSelections() {
+    this.setState({ selectedFoodChoices: [] });
+  }
+
+  nextChoicesPage() {
+    const { choicesPageIndex, foodChoices } = this.state;
+    if (choicesPageIndex < Math.ceil(foodChoices.length / 2) - 1) {
+      this.setState({ choicesPageIndex: choicesPageIndex + 1 });
+    } else {
+      this.setState({ choicesPageIndex: 0 });
+    }
+  }
+
+  lastChoicesPage() {
+    const { choicesPageIndex, foodChoices } = this.state;
+    if (choicesPageIndex !== 0) {
+      this.setState({ choicesPageIndex: choicesPageIndex - 1 });
+    } else {
+      this.setState({ choicesPageIndex: Math.ceil(foodChoices.length / 2) - 1 });
+    }
+  }
+
+  async fetchNearestPlacesFromGoogle() {
+    const { lobbyData } = this.props;
+    
+    const latitude = lobbyData.location.latitude;
+    const longitude = lobbyData.location.longitude;
+    const radius = Math.round(lobbyData.distance * 1609.344);
+    const types = 'restaurant';
+    const url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?'
+      + 'location=' + latitude + ',' + longitude
+      + '&radius=' + radius
+      + '&type=' + types
+      // + '&keyword=restaurant'
+      + '&key=' + NEARBY_SEARCH_API_KEY;
+    
+    this.setState({ loading: true });
+
+    await fetch(url)
+      .then(res => {
+        return res.json();
+      })
+      .then(res => {
+        var places = [];
+        const GooglePicBaseUrl = `https://maps.googleapis.com/maps/api/place/photo?key=${PLACE_DETAILS_API_KEY}&maxwidth=400&photo_reference=`;
+        for(let googlePlace of res.results) {
+          var place = {};
+          const coordinate = {
+            latitude: googlePlace.geometry.location.lat,
+            longitude: googlePlace.geometry.location.lng,
+          };
+
+          var gallery = [];
+          if (googlePlace.photos) {
+            for(let photo of googlePlace.photos) {
+              var photoUrl = GooglePicBaseUrl + photo.photo_reference;
+              gallery.push(photoUrl);
+            }
+          }
+
+          place['types'] = googlePlace.types;
+          place['coordinate'] = coordinate;
+          place['id'] = googlePlace.place_id;
+          place['name'] = googlePlace.name;
+          place['rating'] = googlePlace.rating ?? 0;
+          place['priceLevel'] = googlePlace.price_level ?? 0;
+          place['vicinity'] = googlePlace.vicinity;
+          place['userRatingsTotal'] = googlePlace.user_ratings_total ?? 0;
+          place['photos'] = gallery;
+          place['opennow'] = googlePlace.opening_hours?.open_now ?? false
+
+          places.push(place);
+        }
+        this.setChoices(places);
+      })
+      .catch(error => {
+        console.error("FoodChoices::fetchNearestPlacesFromGoogle", error);
+        this.setChoices([]);
+      })
+      .finally(() => {
+        this.setState({ loading: false })
+      });
+  }
+
+  async getUserFoodSelections() {
+    const { lobbyData, user, db } = this.props;
+
+    try {
+      const selectedFoodChoicesDoc = await getDoc(doc(db, 'food_selections', `${lobbyData.ref.id}_${user.uid}`));
+      const selectedFoodChoices = selectedFoodChoicesDoc.data()?.selections;
+      this.setState({ selectedFoodChoices: selectedFoodChoices ?? [] });
+    } catch (err) {
+      console.error("MakeSelections::getUserFoodSelections", err);
+    }
+  }
+
+  render() {
+    const {
+      screenHeight,
+      googleSearchText,
+      choicesPageIndex,
+      selectedFoodChoices,
+      nextPageToken,
+      foodChoices,
+      loading,
+      maxNumberOfSelections,
+    } = this.state;
+
+    const { user } = this.props;
+
+    const { lobbyData } = this.props;
+
+    const addressName = lobbyData.locationGeocodeAddress &&
+      lobbyData.locationGeocodeAddress[0]?.city + ", " + 
+      lobbyData.locationGeocodeAddress[0]?.region;
+
+    return (
+      <HeaderHeightContext.Consumer>
+        {headerHeight => (
+          <View
+            key={'make-selections-view'}
+            style={{
+              height: screenHeight - headerHeight,
+              paddingHorizontal: 10,
+              justifyContent: 'space-between',
+            }}
+          >
+            <Text ellipsizeMode="tail" style={{ margin: 10, textAlign: 'center', fontSize: 24, fontWeight: 'normal' }}>{addressName}</Text>
+            {/* <FoodProfile
+              {...this.props}
+              selectedFoodProfile={this.setSelectedFoodProfile}
+            /> */}
+            {/* <GoogleFoodSearch
+              {...this.props}
+              googleSearchText={this.setGoogleSearchText}
+            /> */}
+            {
+              loading ? (
+                <LoadingSpinner />
+              ) : (
+                <FoodChoices
+                  {...this.props}
+                  googleSearchText={googleSearchText}
+                  removeFoodChoice={this.removeFoodChoice}
+                  addFoodChoice={this.addFoodChoice}
+                  choicesPageIndex={choicesPageIndex}
+                  setChoices={this.setChoices}
+                  nextPageToken={nextPageToken}
+                  foodChoices={foodChoices}
+                  selectedFoodChoices={selectedFoodChoices}
+                  lobbyData={lobbyData}
+                  maxNumberOfSelections={maxNumberOfSelections}
+                />
+              )
+            }
+            <View>
+              <View style={{ justifyContent: 'space-between', flexDirection: 'row', marginTop: -8, paddingHorizontal: 15 }}>
+                <Text>{''}</Text>
+                <Text>Page {choicesPageIndex}</Text>
+              </View>
+              <FoodPageNavigation
+                {...this.props}
+                nextChoicesPage={this.nextChoicesPage}
+                lastChoicesPage={this.lastChoicesPage}
+                choicesPageIndex={choicesPageIndex}
+                clearSelections={this.clearSelections}
+                selectedFoodChoices={selectedFoodChoices}
+                lobbyData={lobbyData}
+                user={user}
+                maxNumberOfSelections={maxNumberOfSelections}
+              />
+            </View>
+          </View>
+        )}
+      </HeaderHeightContext.Consumer>
+    )
+  }
+}
+
+export default MakeSelections;
