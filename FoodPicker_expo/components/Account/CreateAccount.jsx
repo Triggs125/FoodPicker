6f4b844d1f9c868b2d242ddff4a75755cc042110
Triggs125 <tannerdriggers@gmail.com>
2022-03-05 12:55:37 -0600

    Revert "Add Sharing"
    This reverts commit 8554528e900b00d730b44c1151312c048c3b150d.

---
+++ FoodPicker_expo/components/Account/CreateAccount.jsx
@@ -0,0 +1,275 @@
+import { Component } from 'react';
+import { SafeAreaView, View } from 'react-native';
+import { Input, Text, Icon, Button } from 'react-native-elements';
+import PasswordValidator from 'password-validator';
+import isAlpha from 'validator/lib/isAlpha';
+import isEmail from 'validator/lib/isEmail';
+// import SignInWithGoogle from '../Utils/SignInWithGoogle';
+import { AddUserToDB } from '../Utils/firebase';
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { ScrollView } from 'react-native-gesture-handler';
+
+class CreateAccount extends Component {
+  constructor(props) {
+    super(props);
+
+    var passwordSchema = new PasswordValidator();
+    passwordSchema
+      .is().min(8, " minimum of 8 characters")
+      .is().max(100, " maximum of 100 characters")
+      .has().uppercase(1, " at least 1 uppercase letter")
+      .has().lowercase(1, " at least 1 lowercase letter")
+      .has().digits(1, " at least 1 number")
+      .has().not().spaces(0, " no spaces");
+
+    this.state = {
+      firstNameError: false,
+      firstNameText: "",
+      lastNameText: "",
+      emailAddressText: "",
+      emailAddressError: false,
+      passwordText: "",
+      passwordFailures: [],
+      passwordShowing: false,
+      passwordValidator: passwordSchema,
+      emailExistsError: false,
+    }
+
+    this.createAccount = this.createAccount.bind(this);
+  }
+
+  async createAccount() {
+    if (this.formValidation()) {
+      console.log("Valid Form");
+      try {
+        const res = await createUserWithEmailAndPassword(this.props.auth, this.state.emailAddressText, this.state.passwordText);
+        const displayName = `${this.state.firstNameText}${this.state.lastNameText !== "" ? " " + this.state.lastNameText : ""}`;
+        await updateProfile(this.props.auth.currentUser, {
+          displayName: displayName
+        });
+        await AddUserToDB(this.props.db, res.user, this.state.firstNameText, this.state.lastNameText, displayName);
+        this.setState({ firstNameText: "", lastNameText: "", emailAddressText: "", passwordText: "", passwordShowing: false });
+        this.props.navigation.navigate('LobbyPicker');
+      } catch (err) {
+        console.error("Account Creation Error:", err);
+        if (err.code === "auth/email-already-in-use") {
+          this.setState({ emailExistsError: true });
+        }
+      }
+    } else {
+      console.log("Invalid Form");
+    }
+  }
+
+  /**
+   * Validates the form inputs
+   * @returns {Boolean} If there were validation errors
+   */
+  formValidation() {
+    const { firstNameText, emailAddressText, passwordText, passwordValidator } = this.state;
+
+    // First Name Validation
+    const validFirstName = isAlpha(firstNameText);
+
+    // Email Validation
+    const validEmailAddress = isEmail(emailAddressText);
+    
+    // Password Validation
+    const passwordFailures = passwordValidator.validate(passwordText, { list: true, details: true });
+
+    this.setState({ passwordFailures: passwordFailures, firstNameError: !validFirstName, emailAddressError: !validEmailAddress });
+
+    return passwordFailures.length === 0 && validFirstName && validEmailAddress;
+  }
+
+  render() {
+    const {
+      firstNameError, 
+      firstNameText,
+      lastNameText,
+      emailAddressError,
+      emailAddressText,
+      passwordText,
+      passwordShowing,
+      passwordFailures,
+      emailExistsError
+    } = this.state;
+
+    return (
+      <SafeAreaView style={{ paddingTop: 10 }}>
+        <ScrollView style={styles.container}>
+          <Input
+            placeholder="First Name *"
+            textContentType="name"
+            leftIcon={
+              <Icon
+                name='user'
+                type='font-awesome'
+                iconStyle={{
+                  ...styles.inputIcon,
+                  marginLeft: 2,
+                  marginRight: 3,
+                }}
+              />
+            }
+            value={firstNameText}
+            inputStyle={styles.inputStyle}
+            containerStyle={{
+              marginTop: 15
+            }}
+            errorMessage={firstNameError ? "Please enter a valid first name" : ""}
+            onChangeText={(text) => this.setState({ firstNameText: text })}
+          />
+          <Input
+            placeholder="Last Name"
+            textContentType="name"
+            value={lastNameText}
+            leftIcon={
+              <Icon
+                name='user'
+                type='font-awesome'
+                iconStyle={{
+                  ...styles.inputIcon,
+                  marginLeft: 2,
+                  marginRight: 3,
+                }}
+              />
+            }
+            inputStyle={styles.inputStyle}
+            onChangeText={(text) => this.setState({ lastNameText: text })}
+          />
+          <Input
+            placeholder="Email Address *"
+            textContentType="emailAddress"
+            autoCapitalize='none'
+            value={emailAddressText}
+            leftIcon={
+              <Icon
+                name='envelope'
+                type='font-awesome'
+                iconStyle={styles.inputIcon}
+              />
+            }
+            inputStyle={styles.inputStyle}
+            errorMessage={emailAddressError ? "Please enter a valid email address" : ""}
+            onChangeText={(text) => this.setState({ emailAddressText: text })}
+          />
+          <Input
+            placeholder="Password *"
+            textContentType="password"
+            autoCapitalize="none"
+            secureTextEntry={!passwordShowing}
+            value={passwordText}
+            leftIcon={
+              <Icon
+                name='key'
+                type='font-awesome'
+                iconStyle={styles.inputIcon}
+              />
+            }
+            rightIcon={
+              <Icon
+                name={passwordShowing ? 'eye-slash' : 'eye'}
+                type='font-awesome'
+                iconStyle={styles.inputIcon}
+                onPress={() => this.setState({ passwordShowing: !passwordShowing })}
+              />
+            }
+            inputStyle={styles.inputStyle}
+            containerStyle={{
+              marginBottom: 5
+            }}
+            errorMessage={passwordFailures.length > 0 ?
+                "Password requirements:" +
+                passwordFailures.flatMap((failure) => {
+                  return failure.message;
+                }) + "."
+                : ""
+            }
+            onChangeText={(text) => this.setState({ passwordText: text })}
+          />
+          {
+            emailExistsError &&
+            <Text
+              style={{
+                textAlign: 'center',
+                fontSize: 16,
+                marginBottom: 10,
+                color: 'red',
+              }}
+            >
+              Email address already in use.
+            </Text>
+          }
+          <Button
+            title="Create Account"
+            raised
+            icon={{
+              name: 'user-plus',
+              type: 'font-awesome',
+              color: 'white',
+              marginRight: 8
+            }}
+            titleStyle={{ fontWeight: '500', fontSize: 22 }}
+            buttonStyle={{
+              backgroundColor: '#E54040',
+              borderColor: 'transparent',
+              borderWidth: 0,
+              height: 60,
+            }}
+            containerStyle={{
+              width: '100%',
+              alignSelf: 'center',
+              marginTop: 0,
+              overflow: 'visible'
+            }}
+            onPress={this.createAccount}
+          />
+          <Button
+            title="Sign In"
+            titleStyle={{
+              textAlign: 'center',
+              fontSize: 20,
+              marginTop: 10,
+              marginBottom: 10,
+              color: '#0645AD',
+            }}
+            onPress={() => this.props.navigation.navigate('Account')}
+          />
+          {/* <Text
+            style={{
+              textAlign: 'center',
+              fontSize: 20,
+              marginTop: 30,
+              marginBottom: 40,
+              color: 'grey',
+            }}
+          >
+            - OR -
+          </Text>
+          <SignInWithGoogle auth={this.props.auth} /> */}
+        </ScrollView>
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = {
+  container: {
+    paddingLeft: 20,
+    paddingRight: 20,
+    height: '100%',
+  },
+  inputIcon: {
+    paddingLeft: 10,
+    color: 'black',
+    fontSize: 24,
+  },
+  inputStyle: {
+    fontSize: 20,
+    paddingLeft: 10,
+  }
+}
+
+export default CreateAccount;
---
+++ FoodPicker_expo/components/Lobby/LobbyPicker.jsx
@@ -0,0 +1,454 @@
+import { Component } from "react";
+import { SafeAreaView, StyleSheet, View } from 'react-native';
+import Constants from 'expo-constants';
+import { Button, Card, Icon, Overlay, Text, Input } from 'react-native-elements';
+import LoadingSpinner from '../LoadingSpinner';
+import { ScrollView, TouchableOpacity } from "react-native-gesture-handler";
+// import ThemeColors from '../../assets/ThemeColors';
+import SearchAlgolia from "../Algolia/SearchAlgolia";
+import { collection, doc, getDocs, onSnapshot, query, setDoc, where, arrayUnion, updateDoc } from 'firebase/firestore';
+import { onAuthStateChanged } from 'firebase/auth';
+import ThemeColors from "../../assets/ThemeColors";
+import { ScreenWidth } from "react-native-elements/dist/helpers";
+import Password from "../Utils/Password";
+
+class LobbyPicker extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false,
+      searchLoading: false,
+      error: false,
+      openOverlay: false,
+      overlayLobby: null,
+      overlayPasswordText: "",
+      overlayPasswordShowing: false,
+      overlayPasswordHash: "",
+      overlayPasswordError: false,
+      kickedMessage: false,
+      removeLobbyOverlay: false,
+      removeLobbyOverlayLobby: null,
+      removeLobbyOverlayLoading: false,
+      removeLobbyOverlayError: false,
+    }
+
+    this.createLobby = this.createLobby.bind(this);
+    this.getLobbyRef = this.getLobbyRef.bind(this);
+    this.lobbyComponent = this.lobbyComponent.bind(this);
+    this.refreshLobbySearch = this.refreshLobbySearch.bind(this);
+  }
+
+  componentDidMount() {
+    onAuthStateChanged(this.props.auth, () => {
+      if (this.state.userLobbiesUnsubscribe) {
+        this.state.userLobbiesUnsubscribe();
+      }
+      const userLobbiesUnsubscribe = this.userLobbiesUnsubscribe();
+      this.setState({ userLobbiesUnsubscribe });
+    });
+    
+    if (this.props.user && !this.state.userLobbies) {
+      const userLobbiesUnsubscribe = this.userLobbiesUnsubscribe();
+      this.setState({ userLobbiesUnsubscribe });
+    }
+
+    this.componentFocusUnsub = this.props.navigation.addListener('focus', () => {
+      if (this.props.route?.params?.kickedFromLobby) {
+        this.setState({ kickedMessage: true });
+      }
+    })
+
+    this.componentBlurUnsub = this.props.navigation.addListener('blur', () => {
+      this.setState({ loading: false });
+      this.state.userLobbiesUnsubscribe && this.state.userLobbiesUnsubscribe();
+    });
+  }
+
+  componentWillUnmount() {
+    const { userLobbiesUnsubscribe } = this.state;
+    userLobbiesUnsubscribe && userLobbiesUnsubscribe();
+  }
+
+  userLobbiesUnsubscribe() {
+    const { user, db } = this.props;
+    return onSnapshot(
+      query(
+        collection(db, 'lobbies'),
+        where('users', 'array-contains', user.uid),
+        where('active', '==', true)
+      ),
+      (lobbies) => {
+        let userLobbies = [];
+        lobbies.forEach((lobby) => {
+          userLobbies.push({ ...lobby.data(), path: lobby.ref.path, id: lobby.ref.id });
+        });
+        this.setState({ userLobbies });
+      },
+      (error) => {
+        console.error("App::onAuthStateChanged", error)
+      },
+      () => { // Auth Complete (ie. user logged out)
+        this.setState({ userLobbiesUnsubscribe: userLobbiesUnsubscribe });
+      }
+    );
+  }
+
+  getLobbyRef(lobby) {
+    return doc(this.props.db, lobby.path);
+  }
+
+  getTitle(lobby) {
+    const userOwned = lobby.host === this.props.user.uid;
+    return (
+      <View style={{ flexDirection: 'row', alignContent: 'center', justifyContent: 'flex-start' }}>
+        {
+          lobby.passwordProtected &&
+          <Icon name="lock" size={18} containerStyle={{ marginRight: 5, alignSelf: 'center' }} />
+        }
+        <Text style={{ ...styles.name, alignSelf: 'center', color: userOwned ? ThemeColors.text : 'black' }}>{lobby.name}</Text>
+      </View>
+    )
+  }
+
+  async addUserToLobby(lobby) {
+    await updateDoc(doc(this.props.db, lobby.path), { users: arrayUnion(this.props.user.uid) });
+  }
+
+  lobbyComponent(lobby, i) {
+    if (!lobby.active) return;
+    return (
+      <Button
+        key={i}
+        title={this.getTitle(lobby)}
+        buttonStyle={{ ...styles.lobbyButton }}
+        containerStyle={{ margin: 1, borderWidth: 0.5, borderColor: 'lightgray' }}
+        raised
+        titleStyle={{ ...styles.name }}
+        icon={lobby?.arrow === false ? <></> : <Icon name="angle-right" type="font-awesome" />}
+        iconRight
+        onPress={async () => {
+          if (lobby.passwordProtected) {
+            try {
+              const passwordHash = (await getDocs(
+                query(
+                  collection(this.props.db, 'lobby_passwords'),
+                  where('lobbyPath', '==', lobby.path)
+                )
+              )).docs[0]?.data().passwordHash;
+              if (passwordHash && passwordHash.length > 0) {
+                this.addUserToLobby(lobby);
+                this.setState({ openOverlay: true, overlayLobby: lobby, overlayPasswordHash: passwordHash });
+              }
+            } catch(err) {
+              console.error("LobbyPicker::lobbyComponent::onPress", err);
+            }
+          } else {
+            this.addUserToLobby(lobby);
+            const lobbyRef = this.getLobbyRef(lobby);
+            this.props.navigation.navigate('LobbyView', { lobbyRef });
+          }
+        }}
+        onLongPress={() => {
+          let isUserLobby = lobby.host === this.props.user.uid
+          if (isUserLobby) {
+            this.setState({ removeLobbyOverlay: true, removeLobbyOverlayLobby: lobby });
+          }
+        }}
+      />
+    );
+  }
+
+  refreshLobbySearch() {
+    this.setState({ refresh: true }, () => {
+      this.setState({ refresh: false });
+    });
+  }
+
+  async createLobby() {
+    this.props.navigation.navigate("LobbyCreator");
+  }
+
+  async removeLobby(lobby) {
+    return setDoc(doc(this.props.db, lobby.path), { active: false }, { merge: true })
+      .then(() => {
+        this.refreshLobbySearch();
+      });
+  }
+
+  render() {
+    const {
+      loading, refresh, userLobbies,
+      openOverlay, overlayLobby, overlayPasswordText,
+      overlayPasswordShowing, overlayPasswordHash, overlayPasswordError,
+      overlayLoading, kickedMessage,
+      removeLobbyOverlay, removeLobbyOverlayLobby, removeLobbyOverlayLoading, removeLobbyOverlayError
+    } = this.state;
+    return (
+      <SafeAreaView>
+        {
+          loading &&
+          <LoadingSpinner />
+        }
+        <Overlay
+          isVisible={removeLobbyOverlay}
+          overlayStyle={{ width: ScreenWidth - 20, borderRadius: 10 }}
+          onBackdropPress={() => {
+            this.setState({
+              removeLobbyOverlay: false,
+              removeLobbyOverlayUser: null,
+              removeLobbyOverlayLoading: false,
+            });
+          }}
+        >
+          <Text
+            style={{ fontSize: 24, textAlign: 'center', marginTop: 10, marginBottom: 20 }}
+          >
+            {`${removeLobbyOverlayLobby?.name}`}
+          </Text>
+          {
+            removeLobbyOverlayError && (
+              <Text>Error removing the user. Please try again or contact support.</Text>
+            )
+          }
+          <Button
+            title="Remove Lobby"
+            loading={removeLobbyOverlayLoading}
+            titleStyle={{ fontSize: 24 }}
+            buttonStyle={{ backgroundColor: ThemeColors.button }}
+            onPress={() => {
+              this.setState({ removeLobbyOverlayLoading: true });
+              this.removeLobby(removeLobbyOverlayLobby)
+                .then(() => {
+                  this.setState({
+                    removeLobbyOverlay: false,
+                    removeLobbyOverlayLobby: null,
+                    removeLobbyOverlayLoading: false,
+                  });
+                })
+                .catch(err => {
+                  console.log("LobbyView::RemoveLobbyOverlay", err);
+                  this.setState({
+                    removeLobbyOverlayLoading: false,
+                    removeLobbyOverlayError: true,
+                  });
+                });
+            }}
+          />
+          <Button
+            title="Cancel"
+            type="clear"
+            disabled={removeLobbyOverlayLoading}
+            titleStyle={{ color: ThemeColors.text, fontSize: 24 }}
+            onPress={() => {
+              this.setState({
+                removeLobbyOverlay: false,
+                removeLobbyOverlayLobby: null,
+              });
+            }}
+          />
+        </Overlay>
+        <Overlay
+          isVisible={kickedMessage}
+          style={{ width: ScreenWidth - 20 }}
+        >
+          <Text style={{ fontSize: 20, marginBottom: 15, textAlign: 'center' }}>You have been kicked from the lobby</Text>
+          <Button
+            title="Continue"
+            titleStyle={{ fontSize: 24 }}
+            buttonStyle={{ backgroundColor: ThemeColors.button }}
+            onPress={() => this.setState({ kickedMessage: false })}
+          />
+        </Overlay>
+        <Overlay
+          isVisible={openOverlay}
+          overlayStyle={{ width: ScreenWidth - 20, borderRadius: 10 }}
+          onBackdropPress={() => {
+            this.setState({
+              openOverlay: false,
+              overlayLobby: null,
+              overlayPasswordText: "",
+              overlayPasswordShowing: false,
+              overlayPasswordHash: "",
+              overlayPasswordError: false,
+            });
+          }}
+        >
+          <Text style={{ fontSize: 24, textAlign: 'center' }}>{overlayLobby?.name}</Text>
+          <Input
+            placeholder="Password"
+            textContentType="password"
+            secureTextEntry={!overlayPasswordShowing}
+            autoCapitalize='none'
+            disabled={overlayLoading}
+            value={overlayPasswordText}
+            leftIcon={
+              <Icon
+                name='key'
+                type='font-awesome-5'
+                size={18}
+              />
+            }
+            rightIcon={
+              <Button
+                title={overlayPasswordShowing ? 'hide' : 'show'}
+                type="clear"
+                titleStyle={{ color: 'gray' }}
+                onPress={() => this.setState({ overlayPasswordShowing: !overlayPasswordShowing })}
+              />
+            }
+            onChangeText={(text) => this.setState({ overlayPasswordText: text })}
+            inputStyle={{ fontSize: 24, paddingLeft: 5 }}
+            containerStyle={{ marginTop: 10 }}
+            errorMessage={overlayPasswordError ? "Incorrect Password. Please try again." : ""}
+          />
+          <Button
+            title="Open Lobby"
+            loading={overlayLoading}
+            onPress={() => {
+              this.setState({ overlayLoading: true, overlayPasswordError: false });
+              this.props.comparePassword(overlayPasswordText, overlayPasswordHash, (err, passwordCorrect) => {
+                if (err) {
+                  console.error("LobbyPicker::Overlay::PasswordCompare", err);
+                  return;
+                }
+                if (passwordCorrect && openOverlay) {
+                  const lobbyRef = this.getLobbyRef(overlayLobby);
+                  this.props.navigation.navigate('LobbyView', { lobbyRef });
+                  this.setState({
+                    openOverlay: false,
+                    overlayLobby: null,
+                    overlayPasswordText: "",
+                    overlayPasswordShowing: false,
+                    overlayPasswordHash: "",
+                    overlayPasswordError: false,
+                  });
+                } else {
+                  this.setState({ overlayPasswordError: true });
+                }
+                this.setState({ overlayLoading: false });
+              })
+            }}
+            titleStyle={{ fontSize: 24 }}
+            buttonStyle={{ backgroundColor: ThemeColors.button }}
+          />
+          <Button
+            title="Cancel"
+            type="clear"
+            titleStyle={{ color: ThemeColors.text, fontSize: 24 }}
+            onPress={() => {
+              this.setState({
+                openOverlay: false,
+                overlayLobby: null,
+                overlayPasswordText: "",
+                overlayPasswordShowing: false,
+                overlayPasswordHash: "",
+                overlayPasswordError: false,
+              });
+            }}
+          />
+        </Overlay>
+        <View style={styles.container}>
+          <Button
+            title="Create a Lobby"
+            disabled={loading}
+            raised
+            icon={{
+              name: 'home',
+              type: 'font-awesome',
+              color: 'white',
+              marginRight: 8
+            }}
+            titleStyle={{ fontWeight: '500', fontSize: 22 }}
+            buttonStyle={{
+              backgroundColor: '#E54040',
+              borderColor: 'transparent',
+              borderWidth: 0,
+              height: 50,
+            }}
+            containerStyle={{
+              width: '100%',
+              alignSelf: 'center',
+              marginTop: 15,
+              overflow: 'visible'
+            }}
+            onPress={this.createLobby}
+          />
+          <ScrollView
+            contentContainerStyle={{ paddingBottom: 10 }}
+          >
+            <Card
+              id="user-lobbies"
+              containerStyle={styles.cardContainer}
+            >
+              <Card.Title style={styles.cardTitle}>Joined Lobbies</Card.Title>
+              {
+                userLobbies && userLobbies.length > 0 &&
+                <Text style={{ fontSize: 12, marginBottom: 2, marginTop: -5, marginLeft: 5 }}>*Hold down your lobby to delete it</Text>
+              }
+              {userLobbies?.map((lobby, i) => {
+                return this.lobbyComponent(lobby, i);
+              })}
+            </Card>
+            <Card
+              id="search-for-lobbies"
+              containerStyle={styles.cardContainer}
+            >
+              <Card.Title style={styles.cardTitle}>All Lobbies</Card.Title>
+              <SearchAlgolia
+                lobbyComponent={this.lobbyComponent}
+                db={this.props.db}
+                refreshHits={this.refreshLobbySearch}
+                refresh={refresh}
+              />
+            </Card>
+          </ScrollView>
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingLeft: 20,
+    paddingRight: 20,
+    height: '100%',
+    display: 'flex',
+    justifyContent: 'flex-start',
+  },
+  button: {
+    width: '100%',
+    marginTop: 20,
+    alignSelf: 'center',
+  },
+  cardContainer: {
+    overflow: 'scroll',
+    backgroundColor: 'white',
+    paddingBottom: 10,
+    marginLeft: 0,
+    marginRight: 0,
+  },
+  cardTitle: {
+    fontSize: 24,
+    color: 'black',
+  },
+  containerStyle: {
+    marginTop: 1,
+    marginBottom: 1,
+  },
+  lobbyButton: {
+    paddingTop: 5,
+    paddingBottom: 5,
+    backgroundColor: 'white',
+    display: 'flex',
+    justifyContent: 'space-between'
+  },
+  name: {
+    textAlign: 'left',
+    fontSize: 18,
+    color: 'black',
+  }
+});
+
+export default Password(LobbyPicker);
