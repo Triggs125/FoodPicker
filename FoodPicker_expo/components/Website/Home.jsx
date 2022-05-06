683a43a1e32783c01cb915b95c2394909e84d384
Triggs125 <tannerdriggers@gmail.com>
2022-05-06 18:09:55 -0500

    Updated website responsiveness

--- FoodPicker_expo/components/Website/Home.jsx
+++ FoodPicker_expo/components/Website/Home.jsx
@@ -3,11 +3,15 @@ import { Card, Text } from "react-native-elements";
 import MobileStoreButton from 'react-mobile-store-button';
 
 export default function Home() {
-  const androidUrl = "";
+  const androidUrl = "https://play.google.com/store/apps/details?id=com.driggs.foodpicker";
   const iosUrl = "https://apps.apple.com/us/app/food-picker-deluxe/id1615230626";
 
   return (
-    <View>
+    <View
+      style={{
+        justifyContent: 'center'
+      }}
+    >
       <View
         style={{
           flexDirection: 'row',
@@ -32,27 +36,31 @@ export default function Home() {
           />
         </View>
       </View>
-      <Card>
+      <Card
+        containerStyle={{
+          maxWidth: "1000px",
+          alignSelf: 'center',
+        }}
+      >
         <Card.Title style={{ fontSize: 24 }}>Contact Information</Card.Title>
         <Card.Divider />
         <View>
           <Text
             style={{
               fontSize: 18,
-              textAlign: 'center'
+              textAlign: 'center',
+              flex: 1
             }}
           >
             Email Address: DriggsSoftware+FoodPicker@gmail.com
           </Text>
         </View>
       </Card>
-      <Card>
-        <Card.Title style={{ fontSize: 24, marginBottom: 8 }}>
-          <a href="https://www.freeprivacypolicy.com/live/3286ca90-3d6e-493d-ba42-456066b3d592" target="_blank">
-            Privacy Policy
-          </a>
-        </Card.Title>
-      </Card>
+      <Card.Title style={{ fontSize: 24, marginBottom: 8, marginTop: 15 }}>
+        <a href="https://www.freeprivacypolicy.com/live/3286ca90-3d6e-493d-ba42-456066b3d592" target="_blank">
+          Privacy Policy
+        </a>
+      </Card.Title>
     </View>
   );
 }
